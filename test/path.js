62775911299c1d2cb6ea6b730ed3d453fbffa12e
KayleePop <34007889+KayleePop@users.noreply.github.com>
2018-08-04 21:01:56 -0500

    Destroy webtorrent clients after tests

--- test/path.js
+++ test/path.js
@@ -2,23 +2,30 @@ const torrentJs = require('../loadjs-torrent')
 const test = require('tape-promise/tape')
 const WebTorrent = require('webtorrent')
 const magnetLinks = require('./shared/magnetLinks.js')
+const destroyClient = require('./shared/destroyClientPromise.js')
 
 test('only the one file specified by path option is loaded', async (t) => {
+  const client = new WebTorrent()
+
   delete window.string
 
-  await torrentJs(magnetLinks.pathTest, {path: 'string.js'})
+  await torrentJs(magnetLinks.pathTest, {client: client, path: 'string.js'})
   t.equals(window.string, 'string loaded')
+  await destroyClient(client)
 })
 
 test('globstar correctly loads entire folder', async (t) => {
+  const client = new WebTorrent()
+
   delete window.string
 
-  await torrentJs(magnetLinks.pathTest, {path: 'folder/**'})
+  await torrentJs(magnetLinks.pathTest, {client: client, path: 'folder/**'})
   t.equals(window.string, 'undefinedloadedloaded')
+  await destroyClient(client)
 })
 
 test('using the same client with opts.client loads files correctly', async (t) => {
-  let client = new WebTorrent()
+  const client = new WebTorrent()
 
   delete window.string
 
@@ -29,11 +36,15 @@ test('using the same client with opts.client loads files correctly', async (t) =
 
   await torrentJs(magnetLinks.pathTest, {path: 'other.js', client: client})
   t.equals(window.string, 'wrong file loaded')
+  await destroyClient(client)
 })
 
 test('using an initial slash should still load the file correctly', async (t) => {
+  const client = new WebTorrent()
+
   delete window.string
 
-  await torrentJs(magnetLinks.pathTest, {path: '/string.js'})
+  await torrentJs(magnetLinks.pathTest, {client: client, path: '/string.js'})
   t.equals(window.string, 'string loaded')
+  await destroyClient(client)
 })
--- test/singleJs.js
+++ test/singleJs.js
@@ -1,10 +1,15 @@
 const torrentJs = require('../loadjs-torrent')
 const test = require('tape-promise/tape')
 const magnetLinks = require('./shared/magnetLinks.js')
+const destroyClient = require('./shared/destroyClientPromise.js')
+const WebTorrent = require('webtorrent')
 
 test('Single Js file should be correctly loaded', async (t) => {
+  const client = new WebTorrent()
+
   delete window.test
 
-  await torrentJs(magnetLinks.windowString)
+  await torrentJs(magnetLinks.windowString, {client: client})
   t.equals(window.test, 'torrent load success')
+  await destroyClient(client)
 })
